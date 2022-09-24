4f5484c00e6f6f89d5a21f8852c09e23e7ec0610
InesaVirsilaite <inesavirs@gmail.com>
2022-09-24 21:34:05 +0300

    test case added

--- Test/ManoDaktarasTest.cs
+++ Test/ManoDaktarasTest.cs
@@ -8,6 +8,7 @@ namespace BaigiamasisDarbasInesa.Test
         [TestCase("165", "65", "23.88", TestName = "Ugis165,svoris65")]
         [TestCase("170", "70", "24.22", TestName = "Ugis170,svoris70")]
         [TestCase("180", "80", "24.69", TestName = "Ugis180,svoris80")]
+        [TestCase("190", "90", "24.93", TestName = "Ugis190,svoris90")]
 
         [Test]
         public static void TestKmiSkaiciuokle(string asmensUgis, string asmensSvoris, string asmensKmi)
